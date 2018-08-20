8a7dce89dbfd2830532d694986cb47955819a165
piotr-kalanski <piotr.kalanski@gmail.com>
2018-08-20 18:04:35 +0200

    - add IAM role for EC2 instance - add airflow installation steps

--- examples/dags/dag_with_execute_lambda_operator.py
+++ examples/dags/dag_with_execute_lambda_operator.py
@@ -12,7 +12,7 @@ with DAG('dag_with_execute_lambda_operator', start_date=datetime(2018, 8, 11)) a
         )
         >> ExecuteLambdaOperator(
             task_id='task_with_execute_lambda_operator',
-            airflow_context_to_lambda_payload=lambda c: {"date": c["execution_date"].strftime('%Y-%m-%d')   },
+            airflow_context_to_lambda_payload=lambda c: {"date": c["execution_date"].strftime('%Y-%m-%d')},
             additional_payload={"ap1": "c1", "ap2": 21},
             lambda_function_name="ItMetricsCalculateSharedAllocationKeysFunction"
         )
